916b35afafc3654d465a4bdc6a2f985d2627dbe4
SiziphiweD <siziphiwes13@gmail.com>
2023-08-10 20:44:10 +0000

    country_users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
--- Create the users table with constraints
+-- Create the users table with constraints and country enumeration
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
+
